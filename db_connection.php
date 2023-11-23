4b8c96569c2b92acfc90b4ceb7f251f977fd5c0c
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2023-11-23 16:35:11 +0100

    create a DB & connect to it ✅

---
+++ db_connection.php
@@ -0,0 +1,26 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password= "";
+
+$conn = new mysqli ($servername, $username, $password);
+
+if ($conn->connect_error){
+    die("Connection failed : " . $conn->connect_error);
+}else{
+    echo "Connected to the Database seccessfuly\n";
+}
+// let's create a new database
+
+$databaseName = "A_Bank";
+$sqlCreateDB = "CREATE DATABASE IF NOT EXISTS $databaseName";
+if ($conn->query($sqlCreateDB) === TRUE){
+    echo "Database created successfully\n";
+}else{
+    echo "Error creating database : " . $conn->error;
+}
+
+#select database 
+$conn->select_db($databaseName);
+
+?>

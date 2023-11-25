9d887182f35300164a2e025dc7ee50832418e1d4
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2023-11-25 17:41:40 +0100

    create the users table

--- create_tables.php
+++ create_tables.php
@@ -2,4 +2,17 @@
 
 include_once "db_connection.php";
 
+$userTable = "
+CREATE TABLE IF NOT EXISTS users  (
+    id INT PRIMARY KEY AUTO_INCREAMENT,
+    username VARCHAR(200) NOT NULL,
+    PASSWORD VARCHAR(255) NOT NULL,
+    address_id INT,
+    CONSTRAINT unique_username UNIQUE (username),
+    CONSTRAINT fk_user_address FOREIGN KEY (address_id) REFERENCES addresses(id)
+);
+";
+
+
+
 ?>

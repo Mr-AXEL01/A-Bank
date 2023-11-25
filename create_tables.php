9e750c486753087333831ef12b146cb97d7ba283
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2023-11-25 17:50:11 +0100

    create a table to handle many to many between users and role

--- create_tables.php
+++ create_tables.php
@@ -21,6 +21,14 @@ CREATE TABLE IF NOT EXISTS roles (
 );
 ";
 
-
+$userRoleTable = "
+CREATE TABLE IF NOT EXISTS user_roles (
+    user_id INT,
+    role_id INT,
+    PRIMARY KEY (user_id, role_id),
+    CONSTRAINT fk_user_roles_user FOREIGN KEY (user_id) REFERENCES users(id),
+    CONSTRAINT fk_user_roles_role FOREIGN KEY (role_id) REFERENCES roles(id)
+);
+";
 
 ?>

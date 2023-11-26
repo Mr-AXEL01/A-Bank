aba1d6542fa7d6293b75c778f9ce4d2b5b7cfd62
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2023-11-26 23:44:52 +0100

    create form

--- users.php
+++ users.php
@@ -19,5 +19,24 @@
 
     $users = getAllUsers();
     ?>
+
+    <div class="max-w-md mx-auto bg-white p-6 rounded shadow-md">
+        <form method="post" action="">
+            <label for="username" class="block text-sm font-medium text-gray-700">Username</label>
+            <input type="text" name="username" id="username" class="mt-1 p-2 border rounded-md w-full">
+
+            <label for="password" class="block text-sm font-medium text-gray-700 mt-4">Password</label>
+            <input type="password" name="password" id="password" class="mt-1 p-2 border rounded-md w-full">
+
+            <label for="address_id" class="block text-sm font-medium text-gray-700 mt-4">Address ID</label>
+            <input type="number" name="address_id" id="address_id" class="mt-1 p-2 border rounded-md w-full">
+
+            <button type="submit" class="mt-4 bg-blue-500 text-white p-2 rounded-md">Create User</button>
+        </form>
+
+        <table class="mt-8 w-full">
+
+        </table>
+    </div>
 </body>
 </html>

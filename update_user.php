8cbca6fc2218a0ff92cad1cd8697f77897476c80
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2023-11-27 17:11:25 +0100

    set an condition to redirect the user

--- update_user.php
+++ update_user.php
@@ -2,9 +2,17 @@
 include_once 'db_connection.php';
 include_once "crud_user.php";
 
-if (isset($_GET['id']) && is_numeric($_GET['id'])){
-    $userId = $GET['id'];
+if (isset($_GET['id']) && is_numeric($_GET['id'])) {
+    $userId = $_GET['id'];
 
     $user = getUserById($userId);
+
+    if (!$user) {
+        header('Location: users.php');
+        exit();
+    }
+} else {
+    header('Location: users.php');
+    exit();
 }
 ?>

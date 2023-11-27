4aa2bc67676f3d7387bb2be81fc61c3770ceccab
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2023-11-27 20:10:17 +0100

    update the UI of bnks

--- banks.php
+++ banks.php
@@ -10,57 +10,22 @@
     <?php
     include_once 'crud_banks.php';
 
-    if($_SERVER['REQUEST_METHOD'] === 'POST'){
-        $name = $_POST['name'];
-        $logo = $_POST['logo'];
-        createBank($name, $logo);
-    }
-
     $banks = getAllBanks();
     ?>
 
     <div class="max-w-md mx-auto bg-white p-6 rounded-md shadow-md">
-        <form method="post" action="">
-            <label for="name" class="block text-sm font-medium text-gray-700">Bank Name</label>
-            <input type="text" name="name" id="name" class="mt-1 p-2 border rounded-md w-full">
+        <h2 class="text-2xl font-semibold text-gray-800 mb-4">Banks</h2>
+        <ul>
+            <?php foreach ($banks as $bank): ?>
+                <li class="mb-2"><?= $bank['name'] ?>
+                    <!-- Add links to update and delete the bank -->
+                    <a href="update_bank.php?bank_id=<?= $bank['id'] ?>" class="text-blue-500 ml-2">Update</a>
+                    <a href="delete_bank.php?bank_id=<?= $bank['id'] ?>" class="text-red-500 ml-2">Delete</a>
+                </li>
+            <?php endforeach; ?>
+        </ul>
 
-            <label for="logo" class="block mt-4 text-sm font-medium text-gray-700">Logo URL</label>
-            <input type="text" name="logo" id="logo" class="mt-1 p-2 border rounded-md w-full">
-
-            <button type="submit" class="mt-4 bg-blue-500 text-white p-2 rounded-md">Create Bank</button>
-        </form>
-
-        <table class="mt-8 w-full">
-            <thead>
-                <tr>
-                    <th class="py-2 px-4 bg-gray-200">ID</th>
-                    <th class="py-2 px-4 bg-gray-200">Name</th>
-                    <th class="py-2 px-4 bg-gray-200">Logo</th>
-                    <th class="py-2 px-4 bg-gray-200">Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php foreach ($banks as $bank): ?>
-                    <tr>
-                        <td class="py-2 px-4"><?= $bank['id'] ?></td>
-                        <td class="py-2 px-4"><?= $bank['name'] ?></td>
-                        <td class="py-2 px-4"><img src="<?= $bank['logo'] ?>" alt="Bank Logo" class="w-16 h-16"></td>
-                        <td class="py-2 px-4">
-                            <form action="update_bank.php" method="post">
-                                <input type="hidden" name="bank_id" value="<?= $bank['id'] ?>">
-                                <input type="hidden" name="new_name" value="new_name_value">
-                                <input type="hidden" name="new_logo" value="new_logo_value">
-                                <button type="submit" class="bg-blue-500 text-white p-2 rounded-md">Update</button>
-                            </form>
-                            <form action="delete_bank.php" method="post">
-                                <input type="hidden" name="bank_id" value="<?= $bank['id'] ?>">
-                                <button type="submit" class="bg-red-500 text-white p-2 rounded-md">Delete</button>
-                            </form>
-                        </td>
-                    </tr>
-                <?php endforeach; ?>
-            </tbody>
-        </table>
+        <a href="add_bank.php" class="mt-4 bg-blue-500 text-white p-2 rounded-md">Add New Bank</a>
     </div>
 </body>
 </html>

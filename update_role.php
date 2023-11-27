1b011c4f9e772a505fc43ac8fc2c5425b18168b5
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2023-11-27 20:40:14 +0100

    create form for updating the roles

--- update_role.php
+++ update_role.php
@@ -23,6 +23,14 @@
 
     <div class="max-w-md mx-auto bg-white p-6 rounded-md shadow-md">
         <h2 class="text-2xl font-semibold text-gray-800 mb-4">Update Role</h2>
+        <form method="post" action="">
+            <input type="hidden" name="role_id" value="<?= $_POST['role_id'] ?>">
+
+            <label for="new_name" class="block text-sm font-medium text-gray-700">New Role Name</label>
+            <input type="text" name="new_name" id="new_name" class="mt-1 p-2 border rounded-md w-full" value="<?= $_POST['role_name'] ?>">
+
+            <button type="submit" class="mt-4 bg-blue-500 text-white p-2 rounded-md">Update Role</button>
+        </form>
     </div>
 </body>
 </html>

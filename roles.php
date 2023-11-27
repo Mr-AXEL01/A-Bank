cb4a56a3ff0b276a30c8e49d3ff03cb211d90484
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2023-11-27 02:11:05 +0100

    create form to create roles

--- roles.php
+++ roles.php
@@ -16,5 +16,14 @@
     
     $roles = getAllRoles();
     ?>
+
+    <div class="max-w-md mx-auto bg-white p-6 rounded-md shadow-md">
+        <form method="post" action="">
+            <label for="name" class="block text-sm font-medium text-gray-700">Role Name</label>
+            <input type="text" name="name" id="name" class="mt-1 p-2 border rounded-md w-full">
+
+            <button type="submit" class="mt-4 bg-blue-500 text-white p-2 rounded-md">Create Role</button>
+        </form>
+    </div>
 </body>
 </html>

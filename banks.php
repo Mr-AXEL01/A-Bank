d38496b125c7084d38ee449e54c2ab91d8d3ffe4
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2023-11-27 11:36:22 +0100

    create a handle form submission for the banks

--- banks.php
+++ banks.php
@@ -6,7 +6,19 @@
     <script src="https://cdn.tailwindcss.com"></script>
     <title>Document</title>
 </head>
-<body>
+<body class="bg-gray-100 p-8">
+    <?php
+    include_once 'crud_banks.php';
+
+    if($_SERVER['REQUEST_METHOD'] === 'POST'){
+        $name = $_POST['name'];
+        $logo = $_POST['logo'];
+        createBank($name, $logo);
+    }
+
+    $banks = getAllBanks();
+    ?>
+
     
 </body>
 </html>

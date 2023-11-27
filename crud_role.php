ab5f0446c3fe6af26d32ece0eaece4b005571380
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2023-11-27 02:01:18 +0100

    create function to handle the update of roles

--- crud_role.php
+++ crud_role.php
@@ -17,3 +17,11 @@ function getAllRoles(){
     return $result->fetch_all(MYSQLI_ASSOC);
 }
 
+function updateRole($roleId , $newName){
+    global $conn;
+    $query = "UPDATE role SET name=? WHERE id=?";
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param("si", $newName, $roleId);
+    return $stmt->execute();
+}
+

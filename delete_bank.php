8cc84239275db15b8beaa9022c9e814d06ca3dc0
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2023-11-27 19:56:36 +0100

    create file for deleting banks

--- delete_bank.php
+++ delete_bank.php
@@ -1,12 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Delete Bank</title>
+</head>
+<body class="bg-gray-100 p-8">
 <?php
-include_once 'crud_banks.php';
+    include_once 'crud_banks.php';
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $bankId = $_POST['bank_id'];
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $bank_id = $_POST['bank_id'];
 
-    deleteBank($bankId);
-}
-
-header("Location: banks.php");
-exit();
-?>
+        deleteBank($bank_id);
+    }
+    ?>
+    
+</body>
+</html>

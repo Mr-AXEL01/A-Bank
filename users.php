801de9c9b677305b089bfff00864edc0ddd9555f
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2023-11-27 16:51:13 +0100

    add button for update and delete actions

--- users.php
+++ users.php
@@ -11,37 +11,36 @@ $users = getAllUsers();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com"></script>
-    <title>User Management</title>
+    <title>User</title>
    
 </head>
 <body class="bg-gray-100 font-sans">
 
 <div class="container mx-auto my-8 p-8 bg-white shadow-md rounded-md">
-    <h1 class="text-3xl font-bold mb-4">User Management</h1>
+    <h1 class="text-3xl font-bold mb-4">Users</h1>
 
-    <table class="w-full border-collapse border border-gray-300">
+    <table class="w-full border border-collapse">
         <thead>
-        <tr class="bg-gray-100">
-            <th class="py-2 px-4 border-b">ID</th>
-            <th class="py-2 px-4 border-b">Username</th>
-            <th class="py-2 px-4 border-b">Password</th>
-            <th class="py-2 px-4 border-b">Address ID</th>
-            <th class="py-2 px-4 border-b">Action</th>
+        <tr class="bg-gray-200">
+            <th class="py-2 px-4 border">ID</th>
+            <th class="py-2 px-4 border">Username</th>
+            <th class="py-2 px-4 border">Password</th>
+            <th class="py-2 px-4 border">Address ID</th>
+            <th class="py-2 px-4 border">Action</th>
         </tr>
         </thead>
         <tbody>
-        <?php
-        $users = getAllUsers();
-        foreach ($users as $user) {
-            echo "<tr class='hover:bg-gray-50'>";
-            echo "<td class='py-2 px-4 border-b'>{$user['id']}</td>";
-            echo "<td class='py-2 px-4 border-b'>{$user['username']}</td>";
-            echo "<td class='py-2 px-4 border-b'>{$user['password']}</td>";
-            echo "<td class='py-2 px-4 border-b'>{$user['address_id']}</td>";
-            echo "<td class='py-2 px-4 border-b'><a href='update_user.php?id={$user['id']}' class='text-blue-500'>Update</a> | <a href='delete_user.php?id={$user['id']}' class='text-red-500'>Delete</a></td>";
-            echo "</tr>";
-        }
-        ?>
+            <?php foreach ($users as $user): ?>
+                <tr>
+                    <td class="py-2 px-4 border"><?= $user['id'] ?></td>
+                    <td class="py-2 px-4 border"><?= $user['username'] ?></td>
+                    <td class="py-2 px-4 border"><?= $user['address_id'] ?></td>
+                    <td class="py-2 px-4 border">
+                        <a href="update_user.php?id=<?= $user['id'] ?>" class="text-blue-500">Update</a>
+                        <a href="delete_user.php?id=<?= $user['id'] ?>" class="text-red-500 ml-2">Delete</a>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
         </tbody>
     </table>
 </div>

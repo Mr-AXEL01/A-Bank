57e3b8a1788930678faeef2a4b04498b83c5b3a8
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2023-11-27 19:48:49 +0100

    create the form of creation of bank

--- add_bank.php
+++ add_bank.php
@@ -6,7 +6,28 @@
     <script src="https://cdn.tailwindcss.com"></script>
     <title>Add Bank</title>
 </head>
-<body>
+<body class="bg-gray-100 p-8">
+    <?php
+    include_once 'crud_banks.php';
     
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $name = $_POST['name'];
+        $logo = $_POST['logo'];
+
+        createBank($name, $logo);
+    }
+    ?>
+
+    <div class="max-w-md mx-auto bg-white p-6 rounded-md shadow-md">
+        <form method="post" action="">
+            <label for="name" class="block text-sm font-medium text-gray-700">Bank Name</label>
+            <input type="text" name="name" id="name" class="mt-1 p-2 border rounded-md w-full">
+
+            <label for="logo" class="block text-sm font-medium text-gray-700 mt-4">Bank Logo</label>
+            <input type="text" name="logo" id="logo" class="mt-1 p-2 border rounded-md w-full">
+
+            <button type="submit" class="mt-4 bg-blue-500 text-white p-2 rounded-md">Create Bank</button>
+        </form>
+    </div>
 </body>
 </html>

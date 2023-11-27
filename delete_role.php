11600f0a5465845b0e61811ba6b82b80f2253517
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2023-11-27 21:30:18 +0100

    create section for deliting roles

--- delete_role.php
+++ delete_role.php
@@ -1,13 +1,27 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Delete Role</title>
+</head>
+<body class="bg-gray-100 p-8">
+    <?php
+    include_once 'crud_roles.php';
 
-include_once 'crud_roles.php';
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $role_id = $_POST['role_id'];
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {  
-    $roleId = $_POST['role_id'];
+        deleteRole($role_id);
 
-    deleteRole($roleId);
-}
+        header("Location: roles.php");
+        exit();
+    }
+    ?>
 
-header("Location: roles.php");
-exit();
-?>
+    <div classs="max-w-md mx-auto bg-white p-6 rounded-md shadow-md">
+        
+    </div>
+</body>
+</html>

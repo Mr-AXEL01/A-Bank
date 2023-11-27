008c3032dcdc74988cbe9e7c6d290c27e3214fa4
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2023-11-27 20:37:53 +0100

    update file of updatie for the role

--- update_role.php
+++ update_role.php
@@ -1,14 +1,26 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Update Role</title>
+</head>
+<body  class="bg-gray-100 p-8">
+    <?php
+    include_once 'crud_roles.php';
 
-include_once 'crud_roles.php';
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $role_id = $_POST['role_id'];
+        $new_name = $_POST['new_name'];
 
-if($_SERVER['REQUEST_METHOD'] === 'POST'){
-    $roleId = $_POST['role_id'];
-    $newName = $_POST['new_name'];
+        updateRole($role_id, $new_name);
 
-    updateRole($roleId, $newName);
-}
+        header("Location: roles.php");
+        exit();
+    }
+    ?>
 
-header("Location: roles.php");
-exit();
-?>
+    
+</body>
+</html>

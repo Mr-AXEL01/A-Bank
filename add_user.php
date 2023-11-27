0cf66e1be34d148e57874b6b3f1ecb616043053c
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2023-11-27 16:35:03 +0100

    create a condition to check the form of submission

--- add_user.php
+++ add_user.php
@@ -2,5 +2,15 @@
 include_once 'db_connection.php';
 include_once 'crud_users.php';
 
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $addressId = $_POST['address_id'];
 
+    if(createUser($username, $password, $addressId)){
+        echo "User created successfully";
+    }else{
+        echo "Failed to add user";
+    }
+}
 ?>

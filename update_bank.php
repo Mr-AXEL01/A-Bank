beee4de2444ad0bc9ba4e8c366541cc9a216f93c
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2023-11-27 19:53:01 +0100

    set up a form for update the form action

--- update_bank.php
+++ update_bank.php
@@ -1,15 +1,35 @@
-<?php
-include_once 'crud_banks.php';
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Update Bank</title>
+</head>
+<body class="bg-gray-100 p-8">
+    <?php
+    include_once 'crud_banks.php';
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    
-    $bankId = $_POST['bank_id'];
-    $newName = $_POST['new_name']; 
-    $newLogo = $_POST['new_logo']; 
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $bank_id = $_POST['bank_id'];
+        $new_name = $_POST['new_name'];
+        $new_logo = $_POST['new_logo'];
 
-    updateBank($bankId, $newName, $newLogo);
-}
+        updateBank($bank_id, $new_name, $new_logo);
+    }
+    ?>
+    <div class="max-w-md mx-auto bg-white p-6 rounded-md shadow-md">
+        <form method="post" action="">
+            <input type="hidden" name="bank_id" value="<?= $_POST['bank_id'] ?>">
 
-header("Location: banks.php");
-exit();
-?>
+            <label for="new_name" class="block text-sm font-medium text-gray-700">New Bank Name</label>
+            <input type="text" name="new_name" id="new_name" class="mt-1 p-2 border rounded-md w-full">
+
+            <label for="new_logo" class="block text-sm font-medium text-gray-700 mt-4">New Bank Logo</label>
+            <input type="text" name="new_logo" id="new_logo" class="mt-1 p-2 border rounded-md w-full">
+
+            <button type="submit" class="mt-4 bg-blue-500 text-white p-2 rounded-md">Update Bank</button>
+        </form>
+    </div>
+</body>
+</html>

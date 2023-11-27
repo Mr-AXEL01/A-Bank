25c75333666f030f96742b2c3ddd3519ee0629cf
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2023-11-27 16:39:21 +0100

    create a form for add  users

--- add_user.php
+++ add_user.php
@@ -14,3 +14,36 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
     }
 }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Document</title>
+</head>
+<body  class="bg-gray-100 font-sans">
+    <div class="container mx-auto my-8 p-8 bg-white shadow-md rounded-md">
+        <h1 class="text-3xl font-bold mb-4">Add User</h1>
+
+        <form action="add_user.php" method="post">
+            <div class="mb-4">
+                <label for="username" class="block text-gray-700">Username:</label>
+                <input type="text" id="username" name="username" class="w-full px-3 py-2 border border-gray-300 rounded-md">
+            </div>
+
+            <div class="mb-4">
+                <label for="password" class="block text-gray-700">Password:</label>
+                <input type="password" id="password" name="password" class="w-full px-3 py-2 border border-gray-300 rounded-md">
+            </div>
+
+            <div class="mb-4">
+                <label for="address_id" class="block text-gray-700">Address ID:</label>
+                <input type="text" id="address_id" name="address_id" class="w-full px-3 py-2 border border-gray-300 rounded-md">
+            </div>
+
+            <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded-md">Add User</button>
+        </form>
+    </div>
+</body>
+</html>

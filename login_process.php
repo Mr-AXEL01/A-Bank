67268e58a6f59d4395c0ece677d797ed42343ea8
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2023-11-28 04:03:32 +0100

    handle the backend for thr login page

---
+++ login_process.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+include_once 'db_connection.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $query = "SELECT id, username, password FROM users WHERE username=?";
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param("s", $username);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $user = $result->fetch_assoc();
+
+    if ($user && password_verify($password, $user['password'])) {
+
+        $_SESSION['user_id'] = $user['id'];
+        $_SESSION['username'] = $user['username'];
+
+        header("Location: dashboard.php");
+        exit();
+    } else {
+        echo "Invalid username or password. Please try again.";
+    }
+
+    $stmt->close();
+    $conn->close();
+}
+?>

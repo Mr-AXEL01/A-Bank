16315c8288aa3d4fa898eef9d038971df21f488e
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2023-11-28 04:31:05 +0100

    debugg some errors

--- dashboard.php
+++ dashboard.php
@@ -17,7 +17,7 @@ $username = $_SESSION['username'];
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com"></script>
-    <title>dashboard</title>
+    <title>Dashboard</title>
 </head>
 <body class="bg-gray-100">
     <nav class="bg-blue-500 p-4 text-white">

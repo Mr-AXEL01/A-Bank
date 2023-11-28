150019ef4b3bba654d15b78c7ed4f411b2bdb4dc
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2023-11-28 03:54:24 +0100

    retrieve the userfrom data base

--- login.php
+++ login.php
@@ -4,5 +4,14 @@ include_once 'db_connection.php';
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
     $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
+
+    $query = "SELECT id, username, password FROM users WHERE username = ?";
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param("s", $username);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $user = $result->fetch_assoc();
+
+    
 }
 ?>

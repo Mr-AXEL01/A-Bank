f3f41d291cbe738404a5aa914bcf459def4329ea
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2023-11-28 03:51:27 +0100

    create a form for register

--- register.php
+++ register.php
@@ -20,3 +20,29 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Registration</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+</head>
+<body class="bg-gray-100 flex items-center justify-center h-screen">
+    <div class="max-w-md w-full bg-white p-8 rounded-md shadow-md">
+        <h2 class="text-2xl font-semibold text-gray-800 mb-4">User Registration</h2>
+        <form method="post" action="" class="space-y-4">
+            <div>
+                <label for="username" class="block text-sm font-medium text-gray-700">Username:</label>
+                <input type="text" name="username" required class="mt-1 p-2 border rounded-md w-full">
+            </div>
+            <div>
+                <label for="password" class="block text-sm font-medium text-gray-700">Password:</label>
+                <input type="password" name="password" required class="mt-1 p-2 border rounded-md w-full">
+            </div>
+            <button type="submit" class="bg-blue-500 text-white p-2 rounded-md w-full">Register</button>
+        </form>
+    </div>
+</body>
+</html>

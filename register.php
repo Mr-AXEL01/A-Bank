1e8312fa7e0fbb13dcd4f669ed3549999e9a50f6
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2023-11-28 03:45:32 +0100

    create a validation in the register file

---
+++ register.php
@@ -0,0 +1,8 @@
+<?php
+include_once 'db_connection.php';
+
+if($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
+    $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
+}
+?>

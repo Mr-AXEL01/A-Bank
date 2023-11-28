77bad100ad5d24178b6033c8e031736b27b35fd2
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2023-11-28 03:46:35 +0100

    create a system for crepting the passwords

--- register.php
+++ register.php
@@ -4,5 +4,8 @@ include_once 'db_connection.php';
 if($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
     $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
+
+    $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+    
 }
 ?>
